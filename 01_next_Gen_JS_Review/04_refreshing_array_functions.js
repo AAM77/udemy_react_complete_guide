3e62a47b4a66ae9d9d2bcdf8afbd26a8c8b53535
github username <Adeel.A.Mhd@gmail.com>
2019-04-28 22:37:43 -0400

    Added other important functions to keep in mind.

--- 01_next_Gen_JS_Review/04_refreshing_array_functions.js
+++ 01_next_Gen_JS_Review/04_refreshing_array_functions.js
@@ -8,3 +8,12 @@ const doubleNumArray = numbers.map((num) => {
 
 console.log(numbers);
 console.log(doubleNumArray);
+
+// map()  => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+// find()  => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+// findIndex()  => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/findIndex
+// filter()  => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+// reduce()  => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce?v=b
+// concat()  => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat?v=b
+// slice()  => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+// splice()  => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
