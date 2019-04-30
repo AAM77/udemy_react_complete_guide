b90fda75f147f0cdec732474b76c133ba4f972b6
github username <Adeel.A.Mhd@gmail.com>
2019-04-29 23:59:04 -0400

    Created a folder and file 'Person' to demonstrate how to create Functional component.

---
+++ 02_React_Base_Features_and_Syntax/react-complete-guide/src/Person/Person.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// we are not importing the { Component } because we are not using
+// or creating a component here. We are creating a function.
+
+const person = () => {
+  return <p>I'm a person!</p>
+}
+
+export default person;
+// this exports the function 'person'
