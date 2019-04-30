abe811c7354a1339c10974e2dfbeffbad4b1c640
github username <Adeel.A.Mhd@gmail.com>
2019-04-29 23:34:29 -0400

    Commented out code to demonstrate what is happening under the hood.

--- 02_React_Base_Features_and_Syntax/react-complete-guide/src/App.js
+++ 02_React_Base_Features_and_Syntax/react-complete-guide/src/App.js
@@ -2,11 +2,22 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-    </div>
+  // return (
+  //   <div className="App">
+  //     <h1>Hi, I'm a React App</h1>
+  //   </div>
+  // );
+
+  // Let's delve a little deeper to understand what JSX is
+  // The code above is compiled to the following code:
+  return React.createElement(
+    'div', null,
+    React.createElement(
+      'h1', null, 'Hi, I\'m a React App!!!'
+    )
   );
+  // React.createElement(args) can take an infinite number number of arguments.
+
 }
 
 export default App;
