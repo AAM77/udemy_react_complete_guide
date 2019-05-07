f3de330cf830b043e5f8d515586d348d7144f829
github username <Adeel.A.Mhd@gmail.com>
2019-05-07 00:06:50 -0400

    Learned how to pass method references between components.

--- 02_React_Base_Features_and_Syntax/react-complete-guide/src/Person/Person.js
+++ 02_React_Base_Features_and_Syntax/react-complete-guide/src/Person/Person.js
@@ -3,10 +3,17 @@ import React from 'react';
 const person = (props) => {
   return (
     <div>
-      <p>I'm {props.name} and I am {props.age} years old!</p>
+      <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
       <p>{props.children}</p>
     </div>
   )
 }
+
+// here we want to trigger a change when we
+// click on the paragraph, but there is an issue:
+// we don't have access to the clickHandler method because.
+// it is in a different file/different class
+// FORTUNATELY, there is a way to pass it as a property
+
 
 export default person;
