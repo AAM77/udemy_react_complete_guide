300a6fa598c248ba212bd58359037406549ab7bb
github username <Adeel.A.Mhd@gmail.com>
2019-06-02 23:16:28 -0400

    Added a deletePersonHandler to delete a person. Passed it as a prop to the Person component via the map function.

--- 03_Working_with_Lists_and_Conditionals/react-complete-guide/src/App.js
+++ 03_Working_with_Lists_and_Conditionals/react-complete-guide/src/App.js
@@ -14,16 +14,6 @@ class App extends Component {
     showPersons: false
   }
 
-  switchNameHandler = (newName) => {
-    this.setState({
-      persons: [
-        { name: newName, age: '28' },
-        { name: 'Manu', age: '29' },
-        { name: 'Stephanie', age: '27' }
-      ]
-    });
-  }
-
   nameChangedHandler = (event) => {
     this.setState({
       persons: [
@@ -37,6 +27,12 @@ class App extends Component {
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
     this.setState({showPersons: !doesShow})
+  }
+
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons})
   }
 
   // in-line styles are scoped to this element/component
@@ -56,9 +52,11 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map( person => {
+          {this.state.persons.map( (person, index) => {
             return(
               <Person
+                click={() => this.deletePersonHandler(index)}
+                key={index}
                 name={ person.name }
                 age={ person.age }
               />
