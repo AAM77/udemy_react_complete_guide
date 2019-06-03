4c3b7b2387b5ac375da479ed1ed03d13d2c762d5
github username <Adeel.A.Mhd@gmail.com>
2019-06-02 23:45:44 -0400

    Updated nameChangedHandler to be more flexible so it can handle dynamic content. Passed this to the Person component.

--- 03_Working_with_Lists_and_Conditionals/react-complete-guide/src/App.js
+++ 03_Working_with_Lists_and_Conditionals/react-complete-guide/src/App.js
@@ -14,14 +14,21 @@ class App extends Component {
     showPersons: false
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Max', age: '28' },
-        { name: event.target.value, age: '29' },
-        { name: 'Stephanie', age: '27' }
-      ]
+  nameChangedHandler = ( event, id ) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
     });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
   }
 
   togglePersonsHandler = () => {
@@ -68,10 +75,11 @@ class App extends Component {
           {this.state.persons.map( (person, index) => {
             return(
               <Person
-                click={() => this.deletePersonHandler(index)}
                 key={ person.id }
                 name={ person.name }
                 age={ person.age }
+                click={() => this.deletePersonHandler(index)}
+                changed={(event) => this.nameChangedHandler(event, person.id)}
               />
             )
           })}
